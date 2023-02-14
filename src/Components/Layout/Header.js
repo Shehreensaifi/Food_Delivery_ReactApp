e8d4601215fb1186e3ee936a3c939632d789d673
Shehreensaifi <shehreensaifi2001@gmail.com>
2023-02-14 16:19:58 +0530

    First commit React Food Delivery App

---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,21 @@
+import { Fragment } from "react";
+import classes from "./Header.module.css";
+import mealImage from "../../Assets/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>Delicious Meals</h1>
+        <HeaderCartButton onClick={props.onShowCart} />
+      </header>
+
+      <div className={classes["main-image"]}>
+        <img src={mealImage} alt="A table full of delicious food" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
